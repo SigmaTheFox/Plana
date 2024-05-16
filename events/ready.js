2db434e6226f3693838ffa974e4d89c128c50417
SigmaTheFox <valerio.donghia@outlook.com>
2024-05-16 22:12:03 +0200

    added cron job to re-download localization/students.json every day in case of updates

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,9 @@
+const cron = require('node-cron');
+
 module.exports = bot => {
 	console.log('Plana is ready');
+
+	cron.schedule('0 0 * * *', () => {
+		require('../modules/getStudents')();
+	});
 };
---
+++ modules/getStudents.js
@@ -0,0 +1,36 @@
+const { createWriteStream } = require('fs');
+const { Readable } = require('stream');
+const { finished } = require('stream/promises');
+
+module.exports = async () => {
+	await students();
+	await Localization();
+};
+
+async function students() {
+	const studentsURL =
+		'https://raw.githubusercontent.com/SchaleDB/SchaleDB/main/data/en/students.min.json';
+
+	try {
+		const students = createWriteStream('./json/students.json');
+		const { body } = await fetch(studentsURL);
+		await finished(Readable.fromWeb(body).pipe(students));
+		students.close();
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+async function Localization() {
+	const localizationURL =
+		'https://raw.githubusercontent.com/SchaleDB/SchaleDB/main/data/en/localization.min.json';
+
+	try {
+		const localization = createWriteStream('./json/localization.json');
+		const { body } = await fetch(localizationURL);
+		await finished(Readable.fromWeb(body).pipe(localization));
+		localization.close();
+	} catch (err) {
+		console.error(err);
+	}
+}
