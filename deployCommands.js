8c34bc731cbcff975fc8b7d784e8977b3166cd28
SigmaTheFox <valerio.donghia@outlook.com>
2024-05-16 20:02:04 +0200

    made reload a private guild command and student a public command

--- deployCommands.js
+++ deployCommands.js
@@ -4,17 +4,25 @@ const { clientID, privateGuildID, token } = require('./settings/config.json');
 const { readdirSync } = require('fs');
 
 const commands = [];
+const privateCommands = [];
 const commandFiles = readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+	if (command.private === true) privateCommands.push(command.data.toJSON());
+	else commands.push(command.data.toJSON());
 }
 
 const rest = new REST().setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientID, privateGuildID), {
+	body: privateCommands,
+})
+	.then(() => console.log('Successfully registered private commands'))
+	.catch(console.error);
+
+rest.put(Routes.applicationCommands(clientID), {
 	body: commands,
 })
-	.then(() => console.log('Successfully registered guild commands'))
+	.then(() => console.log('Successfully registered commands'))
 	.catch(console.error);
