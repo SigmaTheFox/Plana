922c007013c8bb406bd71b537ab444e33289fb80
SigmaTheFox <valerio.donghia@outlook.com>
2024-05-17 08:44:19 +0200

    initialized pages

--- commands/student.js
+++ commands/student.js
@@ -4,12 +4,15 @@ const {
 	CommandInteraction,
 	EmbedBuilder,
 	AutocompleteInteraction,
+	ButtonBuilder,
+	ButtonStyle,
+	ActionRowBuilder,
 } = require('discord.js');
 const { readFileSync } = require('fs');
 
-const localization = require('../json/localization.json');
 const emotes = require('../json/emotes.json');
 
+let localization = JSON.parse(readFileSync('./json/localization.json'));
 let students = JSON.parse(readFileSync('./json/students.json'));
 let validStudentNames = students.map(student => student.Name);
 
@@ -42,57 +45,123 @@ module.exports = {
 	 * @param {CommandInteraction} interaction
 	 */
 	async execute(bot, interaction) {
-		const userInput = interaction.options.getString('name');
-		const student = students.find(student => student.Name === userInput);
+		const backButton = new ButtonBuilder()
+			.setStyle(ButtonStyle.Secondary)
+			.setLabel('Back')
+			.setEmoji('⬅️')
+			.setCustomId('back');
+		const forwardButton = new ButtonBuilder()
+			.setStyle(ButtonStyle.Secondary)
+			.setLabel('Forwards')
+			.setEmoji('➡️')
+			.setCustomId('forward');
 
-		const embedPage1 = new EmbedBuilder()
+		const userInput = interaction.options.getString('name');
+		const student = students.find(
+			student => student.Name.toLowerCase() === userInput.toLowerCase()
+		);
+
+		const embed = new EmbedBuilder()
 			.setThumbnail(
 				`https://raw.githubusercontent.com/SchaleDB/SchaleDB/main/images/student/icon/${student.Id}.webp`
 			)
-			.setTitle(student.Name)
-			.addFields([
-				{ name: 'Rarity', value: '⭐'.repeat(student.StarGrade), inline: true },
-				{ name: 'School', value: localization['School'][student.School], inline: true },
-				{ name: 'Club', value: localization['Club'][student.Club], inline: true },
-				{
-					name: 'Squad Type',
-					value: localization['SquadType'][student.SquadType],
-					inline: true,
-				},
-				{
-					name: 'Role',
-					value: localization['TacticRole'][student.TacticRole],
-					inline: true,
-				},
-				{ name: 'Position', value: student.Position, inline: true },
-				{
-					name: 'Attack Type',
-					value: localization['BulletType'][student.BulletType],
-					inline: true,
-				},
-				{
-					name: 'Armor Type',
-					value: localization['ArmorType'][student.ArmorType],
-					inline: true,
-				},
-				{ name: '\u200b', value: '\u200b', inline: true },
-				{
-					name: 'Urban',
-					value: emotes['affinity' + student.StreetBattleAdaptation],
-					inline: true,
-				},
-				{
-					name: 'Outdoor',
-					value: emotes['affinity' + student.OutdoorBattleAdaptation],
-					inline: true,
-				},
-				{
-					name: 'Indoor',
-					value: emotes['affinity' + student.IndoorBattleAdaptation],
-					inline: true,
-				},
-			]);
+			.setTitle(student.Name);
 
-		interaction.reply({ embeds: [embedPage1] });
+		const msg = await interaction.reply({
+			embeds: [embed.setFields(embedPages(student)[0]).setFooter({ text: 'Page 1' })],
+			components: [new ActionRowBuilder().addComponents([forwardButton])],
+		});
+
+		let filter = ({ user }) => user.id === interaction.user.id;
+		let collector = msg.createMessageComponentCollector({
+			filter,
+			time: 60000,
+		});
+
+		let pageIndex = 0;
+		collector.on('collect', async interaction => {
+			interaction.customId === 'back' ? (pageIndex -= 1) : (pageIndex += 1);
+			await interaction.update({
+				embeds: [
+					embed
+						.setFields(embedPages(student)[pageIndex])
+						.setFooter({ text: `Page ${pageIndex + 1}` }),
+				],
+				components: [
+					new ActionRowBuilder().addComponents([
+						...(pageIndex ? [backButton] : []),
+						...(pageIndex + 1 < 3 ? [forwardButton] : []),
+					]),
+				],
+			});
+		});
+		collector.on('end', () => {
+			msg.edit({ components: [] }).catch(() => {});
+		});
 	},
 };
+
+function embedPages(student) {
+	const pages = {
+		0: [
+			{ name: 'Rarity', value: '⭐'.repeat(student.StarGrade), inline: true },
+			{ name: 'School', value: localization['School'][student.School], inline: true },
+			{ name: 'Club', value: localization['Club'][student.Club], inline: true },
+			{
+				name: 'Squad Type',
+				value: localization['SquadType'][student.SquadType],
+				inline: true,
+			},
+			{
+				name: 'Role',
+				value: localization['TacticRole'][student.TacticRole],
+				inline: true,
+			},
+			{ name: 'Position', value: student.Position, inline: true },
+			{
+				name: 'Attack Type',
+				value: localization['BulletType'][student.BulletType],
+				inline: true,
+			},
+			{
+				name: 'Armor Type',
+				value: localization['ArmorType'][student.ArmorType],
+				inline: true,
+			},
+			{ name: '\u200b', value: '\u200b', inline: true },
+			{
+				name: 'Urban',
+				value: emotes['affinity' + student.StreetBattleAdaptation],
+				inline: true,
+			},
+			{
+				name: 'Outdoor',
+				value: emotes['affinity' + student.OutdoorBattleAdaptation],
+				inline: true,
+			},
+			{
+				name: 'Indoor',
+				value: emotes['affinity' + student.IndoorBattleAdaptation],
+				inline: true,
+			},
+			{
+				name: 'Equipment',
+				value: student.Equipment.join(', '),
+			},
+		],
+		1: [
+			{
+				name: 'page',
+				value: '1',
+			},
+		],
+		2: [
+			{
+				name: 'page',
+				value: '2',
+			},
+		],
+	};
+
+	return pages;
+}
